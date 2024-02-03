47e071cd944e5714ce951e4bfad7c5dcb2038f18
Nwokorie Alex V <nwokoriealex20@gmail.com>
2024-02-03 14:22:31 +0100

    feat: worked on the admin dashboard page

--- app/Http/Controllers/Admin/AdminPanelController.php
+++ app/Http/Controllers/Admin/AdminPanelController.php
@@ -2,15 +2,46 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\Tenant;
+use App\Models\Remittance;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\ServiceCharge;
 use Illuminate\Support\Facades\Auth;
 
 class AdminPanelController extends Controller
 {
+
+    /**
+     * Display a listing of the resource.
+     */
     public function dashboard()
     {
-        return view('web.backend.dashboard.index');
+        // numbers of tenants
+        $tenants = Tenant::get();
+
+        // numbers of remittance
+        $remittances = Remittance::get();
+
+        // numbers of service charge
+        $service_charges = ServiceCharge::get();
+
+        // countdown
+        $now = Carbon::now();
+        $twoWeeksFromNow = $now->copy()->addWeeks(2);
+
+        $countdowns = Remittance::whereBetween('rent_due_date', [$now, $twoWeeksFromNow])
+            ->orderBy('rent_due_date')
+            ->simplePaginate(5);
+
+        // check overdue
+        $overdues = Remittance::where('rent_due_date', '<', $now)
+            ->orderBy('rent_due_date')
+            ->simplePaginate(5);
+
+        return view('web.backend.dashboard.index', compact('tenants', 'remittances', 'service_charges',
+                    'countdowns', 'now', 'overdues'));
     }
 
     public function logout(Request $request)
--- resources/views/web/backend/dashboard/index.blade.php
+++ resources/views/web/backend/dashboard/index.blade.php
@@ -14,10 +14,10 @@
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Total Admin</h4>
+                            <h4>Total Tenants</h4>
                         </div>
                         <div class="card-body">
-                            NaN
+                            {{ $tenants->count() }}
                         </div>
                     </div>
                 </div>
@@ -29,10 +29,10 @@
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>News</h4>
+                            <h4>Total Remittance</h4>
                         </div>
                         <div class="card-body">
-                            NaN
+                            {{ $remittances->count() }}
                         </div>
                     </div>
                 </div>
@@ -44,10 +44,10 @@
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Reports</h4>
+                            <h4>Total Service Charge</h4>
                         </div>
                         <div class="card-body">
-                            NaN
+                            {{ $service_charges->count() }}
                         </div>
                     </div>
                 </div>
@@ -59,13 +59,107 @@
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Online Users</h4>
+                            <h4>Total Overdue</h4>
                         </div>
                         <div class="card-body">
-                            NaN
+                            {{ $overdues->count() }}
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="card card-warning">
+            <div class="card-header">
+                <h4>Duration of Rent Due to all Tenants!</h4>
+            </div>
+
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr style="text-align: center;">
+                            <th>#ID</th>
+                            <th>Tenant Names</th>
+                            <th>Apartment</th>
+                            <th>Due Date</th>
+                            <th>Countdown</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($countdowns->isEmpty())
+                            <p>No tenant's rent is close to being overdue yet.</p>
+                        @else
+                            @foreach ($countdowns as $countdown)
+                            <tr style="text-align: center;">
+                                <td><a>{{ $countdown->id }}</a></td>
+                                <td><a>{{ $countdown->tenant_name }}</a></td>
+                                <td class="font-weight-600">{{ $countdown->apartment }}</td>
+                                <td>{{ $countdown->rent_due_date }}</td>
+                                <td>
+                                    @php
+                                        $dueDate = Carbon\Carbon::parse($countdown->rent_due_date);
+                                        $the_countdown = $now->diffInHours($dueDate);
+
+                                        // If less than a day, display hours
+                                        if ($the_countdown < 24) {
+                                            echo $the_countdown . ' hours';
+                                        } else {
+                                            echo $now->diffInDays($dueDate) . ' days';
+                                        }
+                                    @endphp
+                                    {{-- {{ $countdown }} days left --}}
+                                </td>
+                            </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+
+                <!-- Simple pagination links -->
+                <div class="pagination" style="margin: 0 auto; justify-content: center; margin-top: 10px;">
+                    {{ $countdowns->links('pagination::simple-bootstrap-4') }}
+                </div>
+
+            </div>
+        </div>
+
+        <div class="card card-warning">
+            <div class="card-header">
+                <h4>All Overdue Tenant Rents!</h4>
+            </div>
+
+            <div class="card-body">
+
+                <table class="table">
+                    <thead>
+                        <tr style="text-align: center;">
+                            <th>#ID</th>
+                            <th>Tenant Names</th>
+                            <th>Apartment</th>
+                            <th>Due Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($overdues->isEmpty())
+                            <p>No tenant's rent is overdue yet.</p>
+                        @else
+                            @foreach ($overdues as $overdue)
+                            <tr style="text-align: center;">
+                                <td><a>{{ $overdue->id }}</a></td>
+                                <td><a>{{ $overdue->tenant_name }}</a></td>
+                                <td class="font-weight-600">{{ $overdue->apartment }}</td>
+                                <td>{{ $overdue->rent_due_date }}</td>
+                            </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+
+                <!-- Simple pagination links -->
+                <div class="pagination" style="margin: 0 auto; justify-content: center; margin-top: 10px;">
+                    {{ $overdues->links('pagination::simple-bootstrap-4') }}
+                </div>
+
             </div>
         </div>
 
